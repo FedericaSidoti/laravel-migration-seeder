47bf25496b1cf208d8835c965b6cde791c7df166
FedericaSidoti <federicasidoti21@gmail.com>
2023-12-13 16:46:25 +0100

    ADD- controller and model, stamp

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index(){
-
-        return view('index');
+        
+        $trains= Train::all();
+        return view('index', compact('trains'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,4 +6,26 @@
 
 @section('content')
     <h1>Homepage</h1>
+    @foreach ($trains as $train)
+        <p>{{$train->company}}</p>
+        <p>{{$train->departure_station}}</p>
+        <p>{{$train->arrival_station}}</p>
+        <p>{{$train->departure_time}}</p>
+        <p>{{$train->arrival_time}}</p>
+        <p>{{$train->number_of_carriages}}</p>
+        <p>Il treno è in orario?</p>
+        @if ($train->is_in_time === 1)
+            <span>Sì</span>
+        @else
+            <span>No </span>
+        @endif
+        <p>Il treno è stato cancellato?</p>
+        
+        @if ($train->is_canceled === 1)
+            <span>Sì</span>
+        @else
+            <span>No </span>
+        @endif
+    @endforeach
+    
 @endsection
